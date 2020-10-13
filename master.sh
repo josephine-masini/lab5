a8429a200d54bee42b60d99078bf86255e2a9c58
josephine <josephinemasini@gmail.com>
2020-10-13 10:36:20 +0000

    question 1

---
+++ master.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+#Listing des fichiers se trouvant dans le répertoire utilisé
+cd home/student/Desktop/lab4 #accès au répertoire
+ls #liste des fichiers
+master_syscom ="Hello Embedded Linux"
+echo "$master_syscom"
+
+
+
+
